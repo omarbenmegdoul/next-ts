8ec42e2371acb4cf92f2ff970e5cc6d3e15e22aa
omarbenmegdoul <omarbenmeg@gmail.com>
2021-12-23 10:01:52 -0500

    added component comments

---
+++ components/inputs.ts
@@ -0,0 +1,19 @@
+// IncrementableNumber
+// Word
+// DeathSave
+// Alignment
+// ProficiencyOptionalMechanic
+// EquippedItem
+// BagItem
+// MiscStatus
+// MiscProficiencies
+// Spell
+// SpellSlots
+
+
+
+
+
+
+
+export { };
