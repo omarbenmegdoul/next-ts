967d65b5f0ecc0337b9ec95ae6489416fb491f4c
omarbenmegdoul <omarbenmeg@gmail.com>
2021-12-23 16:34:41 -0500

    progress on interfaces

--- components/inputs.ts
+++ components/inputs.ts
@@ -10,10 +10,10 @@
 // Spell
 // SpellSlots
 
+export const [IncrementableNumberComp, WordComp, SelectionComp, DeathSaveComp, AlignmentComp, ProficiencyOptionalMechanicComp, EquippedItemComp, BagItemComp, MiscStatusComp, MiscProficienciesComp, SpellComp, SpellSlotsComp] =
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map(() => { return (): number => { return 1 } })
 
 
 
 
 
-
-export { };
--- dataModels/interfaces.ts
+++ dataModels/interfaces.ts
@@ -1,6 +1,16 @@
-export interface stat {
-  modifier: string; value: number
-}
+import { IncrementableNumberComp, WordComp, DeathSaveComp, AlignmentComp, ProficiencyOptionalMechanicComp, EquippedItemComp, BagItemComp, MiscStatusComp, MiscProficienciesComp, SpellComp, SpellSlotsComp } from "../components/inputs";
+
+// export class stat {
+//   score: number;
+//   modifier: number;
+//   comp: 
+
+//   constructor(stat : number) {
+//     this.score = stat;
+
+//   }
+//   modifier: string; value: number
+// }
 
 export interface stats {
   strength: stat;
@@ -10,61 +20,89 @@ export interface stats {
   wisdom: stat;
   charisma: stat;
 }
-interface skill { proficient: boolean; score: number }
 
+interface GenericAttribute { nice: string, component: () => number; }
+
+export interface ProficiencyOptionalMechanic extends GenericAttribute {
+  proficient: boolean;
+  modifier: number;
+
+}
+interface NullableIncrementableNumber extends GenericAttribute {
+  val: number | null;
+}
+interface IncrementableNumber extends GenericAttribute {
+  val: number;
+}
+
+interface Slots extends GenericAttribute {
+  val: number;
+  cap: number;
+}
 export interface skills {
-  athletics: skill;
-  acrobatics: skill;
-  sleight: skill;
-  stealth: skill;
-  arcana: skill;
-  history: skill;
-  investigation: skill;
-  nature: skill;
-  religion: skill;
-  animal: skill;
-  insight: skill;
-  medicine: skill;
-  perception: skill;
-  survival: skill;
-  deception: skill;
-  intimidation: skill;
-  performance: skill;
-  persuasion: skill;
+  athletics: ProficiencyOptionalMechanic;
+  acrobatics: ProficiencyOptionalMechanic;
+  sleight: ProficiencyOptionalMechanic;
+  stealth: ProficiencyOptionalMechanic;
+  arcana: ProficiencyOptionalMechanic;
+  history: ProficiencyOptionalMechanic;
+  investigation: ProficiencyOptionalMechanic;
+  nature: ProficiencyOptionalMechanic;
+  religion: ProficiencyOptionalMechanic;
+  animal: ProficiencyOptionalMechanic;
+  insight: ProficiencyOptionalMechanic;
+  medicine: ProficiencyOptionalMechanic;
+  perception: ProficiencyOptionalMechanic;
+  survival: ProficiencyOptionalMechanic;
+  deception: ProficiencyOptionalMechanic;
+  intimidation: ProficiencyOptionalMechanic;
+  performance: ProficiencyOptionalMechanic;
+  persuasion: ProficiencyOptionalMechanic;
 }
 
 export interface hp {
-  max: number;
-  current: number;
-  temp: number;
+  max: IncrementableNumber;
+  current: IncrementableNumber;
+  temp: IncrementableNumber;
 }
-
 export interface survival {
   hp: hp;
-  ac: number;
+  ac: IncrementableNumber;
   deathSaves: {
-    s: 0 | 1 | 2 | 3;
-    f: 0 | 1 | 2 | 3;
+    s: Slots;
+    f: Slots;
   }
 }
 
 export interface combatSpeed {
-  initiative: null | number;
-  speed: number;
+  initiative: NullableIncrementableNumber;
+  speed: IncrementableNumber;
 }
 
+export interface Word extends GenericAttribute {
+  val: string;
+}
 
+export interface Selection extends GenericAttribute {
+  allowed: string[];
+  current: string;
+}
 export interface identity {
-  name: string;
-  class: string; //TODO restrict to actual classes?
-  race: string;
-  alignment: ["Lawful" | "Neutral" | "Chaotic", "Good" | "Neutral" | "Evil"];
+  name: Word;
+  class: Selection; //TODO restrict to actual classes?
+  race: Selection;
+  alignment: {
+    order: Selection;
+    good: Selection;
+  }
 }
-
 export interface progression {
-  experience: number;
-  level: number;
-  proficiencyBonus: number;
+  experience: IncrementableNumber;
+  level: IncrementableNumber;
+  proficiencyBonus: IncrementableNumber;
+}
+export interface List extends GenericAttribute {
+
 }
 
 export interface character {
